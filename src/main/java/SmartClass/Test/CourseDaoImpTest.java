be12bff6617a75cfb6deae6200beeadac6248569
hubiao <736813089@qq.com>
2018-01-31 20:25:50 +0800

    解决了hibernate中多对多和一对多关系

--- src/main/java/SmartClass/Test/CourseDaoImpTest.java
+++ src/main/java/SmartClass/Test/CourseDaoImpTest.java
@@ -2,11 +2,17 @@ package SmartClass.Test;
 
 import SmartClass.Dao.CourseDao;
 import SmartClass.DaoImp.CourseDaoImp;
+import SmartClass.DaoImp.StudentDaoImp;
 import SmartClass.DaoImp.TeacherDaoImp;
+import SmartClass.HibernateSessionFactory;
 import SmartClass.POJO.Course;
+import SmartClass.POJO.Student;
 import SmartClass.POJO.Teacher;
+import SmartClass.dbutil.AfDbUtil;
+import org.hibernate.Session;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -21,15 +27,39 @@ public class CourseDaoImpTest
 
         try
         {
+            StudentDaoImp studentDaoImp = new StudentDaoImp();
             TeacherDaoImp teacherDaoImp = new TeacherDaoImp();
-            Teacher teacher = teacherDaoImp.getById((short)1006);
+            Teacher teacher = teacherDaoImp.getById((short)1030);
+
+//            Teacher teacher = new Teacher();
+//            teacher.setCode("20171007");
+//            teacher.setName("dai");
+//            teacher.setPassword("111100");
 
             Course course = new Course();
-            course.setCourseName("交换原理");
+            course.setCourseName("计算机网络c班");
             course.setAnswerFlag((byte)0);
             course.setCheckinFlag((byte)0);
             course.setTeacherByTeacherId(teacher);
+
+            Student student1 = new Student();
+            student1.setCode("10002");
+            student1.setName("小王2");
+            student1.setPassword("123456");
+
+            Student student2 = new Student();
+            student2.setCode("10003");
+            student2.setName("老王3");
+            student2.setPassword("123456");
+
+//            Student student1 =  studentDaoImp.getById((short)1014);
+            HashSet<Student> stus = new HashSet<Student>();
+            stus.add(student1);
+            stus.add(student2);
+            course.setStudents(stus);
             courseDaoImp.save(course);
+            System.out.println("hello");
+
         } catch (Exception e)
         {
             e.printStackTrace();
@@ -59,6 +89,34 @@ public class CourseDaoImpTest
             {
                 System.out.println(c.getCourseName());
             }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void getByIdTest()
+    {
+        try
+        {
+            Course course = courseDaoImp.getById((short)1018);
+            System.out.println(course.getCourseName() + course.getTeacherByTeacherId().getName());
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void updateTest()
+    {
+        try
+        {
+
+            //courseDaoImp.update();
         } catch (Exception e)
         {
             e.printStackTrace();
